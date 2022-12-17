63509d6006ada635df31d76d55a276eccb53709a
uelSmall <uelsmall08@gmail.com>
2022-12-17 13:41:08 -0400

    Renew project

--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import "../app.css";
-</script>
-
-<section>
-    <button class="bg-blue-600 mb-2 mt-2 rounded-sm px-4 py-2"  onclick="TogetherJS(this); return false;">Start TogetherJS</button>
-
-</section>
